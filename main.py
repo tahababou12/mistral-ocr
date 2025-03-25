0f9f1bb98f4973129ea9f13483463dde7a7fcf84
Taha Ababou <hababou@bu.edu>
2025-03-25 12:05:07 -0400

    mistral init

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ from mistralai import Mistral
 
 st.set_page_config(layout="wide", page_title="Mistral OCR App", page_icon="🖥️")
 st.title("Mistral OCR App")
-st.markdown("<h3 style color: white;'>Built by <a href='https://github.com/AIAnytime'>AI Anytime with ❤️ </a></h3>", unsafe_allow_html=True)
 with st.expander("Expand Me"):
     st.markdown("""
-    This application allows you to extract information from pdf/image based on Mistral OCR. Built by AI Anytime.
+    This application allows you to extract information from pdf/image based on Mistral OCR.
     """)
 
 # 1. API Key Input
